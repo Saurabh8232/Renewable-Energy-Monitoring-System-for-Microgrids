a033ad641c41261f0b42d7e8f47b60d764656fc8
Arpit <arpit.gangwar061@gmail.com>
2025-08-30 17:53:04 +0530

    trying to connect to renden.com

---
+++ python_script.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/esp32-data', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    print("📩 Data received from ESP32:", data)
+    return jsonify({"status": "success", "data": data}), 200
+
+@app.route('/')
+def home():
+    return "ESP32 Data Receiver is running 🚀"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
