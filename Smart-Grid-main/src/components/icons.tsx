eb92de56a9795832e207a9164972913a827bad85
Saurabh Yadav <sy962965@gmail.com>
2025-09-12 00:20:23 +0530

    Delete Smart-Grid-main directory

--- Smart-Grid-main/src/components/icons.tsx
+++
@@ -1,8 +0,0 @@
-import { Zap } from 'lucide-react';
-import type { SVGProps } from 'react';
-
-export const Icons = {
-  logo: (props: SVGProps<SVGSVGElement>) => (
-    <Zap {...props} />
-  ),
-};
